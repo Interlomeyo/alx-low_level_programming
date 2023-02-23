d527b01db60f4b0a81d2e0a2e46d68cdb6a17534
interlomeyo <interlomeyo@gmail.com>
2023-02-22 16:30:59 -0800

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets
+ *
+ * Return: alwauys 0;
+ */
 
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	_putchar(i);
+	_putchar('\n');
+}
