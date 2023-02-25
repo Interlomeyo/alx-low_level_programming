ed0d56ab285b7597eea6c0a41bb68f6477c2c6d6
interlomeyo <interlomeyo@gmail.com>
2023-02-24 16:11:14 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letter
+ * @c: char to be checked
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
